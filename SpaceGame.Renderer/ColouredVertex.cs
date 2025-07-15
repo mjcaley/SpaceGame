3995720518700d535486917bc9773d626718a459
Michael Caley <mjcaley@darkarctic.com>
2025-07-14 20:37:57 -0400

    Added layout for ColouredVertex

--- SpaceGame.Renderer/ColouredVertex.cs
+++ SpaceGame.Renderer/ColouredVertex.cs
@@ -1,7 +1,9 @@
 using System.Numerics;
+using System.Runtime.InteropServices;
 
 namespace SpaceGame.Renderer;
 
+[StructLayout(LayoutKind.Sequential)]
 public struct ColouredVertex(Vector2 position, Vector4 colour)
 {
     public Vector2 Position { get; set; } = position;
