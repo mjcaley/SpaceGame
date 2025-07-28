f4cd279c1d75f6e59c336f8a8d0cf558ea5ed8e4
Michael Caley <mjcaley@darkarctic.com>
2025-07-28 11:33:18 -0400

    Ensured matrix layout is column major

--- SpaceGame.Build.Shaders/FormatExtensions.cs
+++ SpaceGame.Build.Shaders/FormatExtensions.cs
@@ -3,7 +3,7 @@ internal static class FormatExtensions
     public static string[] AsCompilerArgs(this Format format, string path) =>
         format switch
         {
-            Format.Spirv => [path, "-target", "spirv", "-emit-spirv-directly", "-fvk-use-entrypoint-name"],
+            Format.Spirv => [path, "-g3", "-matrix-layout-column-major", "-target", "spirv", "-emit-spirv-directly", "-fvk-use-entrypoint-name"],
             _ => throw new ArgumentException($"Format {format} not supported")
         };
 }
